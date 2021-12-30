a62f494374a294bee4a0bb33f6cfaa8e23c3220e
John Sun <62114614+iohnS@users.noreply.github.com>
2021-12-30 02:18:08 +0100

    change RGUI = coding layer

--- trikl/keymaps/iohnS/keymap.c
+++ trikl/keymaps/iohnS/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LT(_NUMBERS, KC_TAB),   KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,  KC_LBRC,  KC_RBRC,                     KC_DEL,  KC_END,  KC_PGDN,
         LT(_NAVIGATION, KC_CAPS),   KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,  KC_L,  KC_SCLN,  KC_QUOT,  KC_NUHS,  KC_ENT,
         KC_LSFT,  KC_NUBS,  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT,             KC_MUTE, KC_UP, _______,
-        KC_LCTL,   KC_LALT,  LT(_CODING, KC_LGUI),                       KC_SPC,                     KC_RALT,  KC_RGUI,  OSL(_MACRO),  KC_RCTL,  KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL,   KC_LGUI,  KC_LALT,                       KC_SPC,                     KC_RALT,  LT(_CODING, KC_RGUI),  OSL(_MACRO),  KC_RCTL,  KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [_CODING] = LAYOUT(
         RESET,          _______, _______, _______, _______,     _______, _______, _______, _______,      _______, _______, _______, _______,   _______, _______, _______,
